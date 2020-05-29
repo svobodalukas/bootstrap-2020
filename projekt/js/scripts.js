ec98316e9266ba6a41bf984845c611ca2110a195
Lukáš Svoboda <info@lukassvoboda.cz>
2020-05-29 16:26:15 +0200

    Update afternoon

--- projekt/js/scripts.js
+++ projekt/js/scripts.js
@@ -1,1 +1,26 @@
 // Custom scripts
+
+var form = document.getElementById('bookForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  event.stopPropagation;
+
+  $.get( "https://www.lukassvoboda.cz/coding/bootstrap2020/ajax.php", $( "#bookForm" ).serialize() ,function( data ) {
+    $( ".result" ).html( data );
+    $( "#bookForm" ).hide();
+  });
+  
+});
+
+// Modal open
+$('#bookModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget);
+  var estateId = button.data('flat');
+
+  console.log('open modal');
+
+  var modal = $(this);
+  modal.find('#bytID').html(estateId);
+  modal.find('#flatID').val(estateId);
+})
+
